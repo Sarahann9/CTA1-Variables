eea4b23aa05964d3d3a5a4c04763774386dee185
Sarah Leflore <sarahleflore@Sarahs-Laptop.local>
2025-01-17 09:12:00 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::string firstName = "First name: John";
+    std::string lastName = "Last Name: Smith";
+    std::string address = "Address: 123 Maple Lane";
+    std::string city = "City: New York,";
+    int zipCode = 98364;
+    std::cout << firstName << '\n' << lastName  << '\n' <<address << '\n' << city << " " << zipCode << std::endl;
+    return 0;
+}
+
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
