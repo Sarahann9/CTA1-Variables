7bd013924679dd0757143be67b834d8f8559466a
Sarahann9 <151089650+Sarahann9@users.noreply.github.com>
2025-01-17 09:13:15 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,3 @@ int main() {
     std::cout << firstName << '\n' << lastName  << '\n' <<address << '\n' << city << " " << zipCode << std::endl;
     return 0;
 }
-
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
